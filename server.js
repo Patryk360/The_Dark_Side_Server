c4ea240b9f6bc8242b181de0e6d1197af1ec7c00
Patryk360 <nexigamingyt@gmail.com>
2023-08-07 15:19:57 +0200

    0.0.3

---
+++ server.js
@@ -0,0 +1,11 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+wss.on("connection", async (ws)=> {
+    ws.on("message", async (message) => {
+        console.log("received: \n %s", message);
+        ws.send(message);
+    });
+});
+wss.on("listening", () => {
+    console.log("Server started...");
+});
