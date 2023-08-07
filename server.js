463afb8f157e55104c78e5c93b713dbc11f6d089
Patryk360 <nexigamingyt@gmail.com>
2023-08-08 01:51:27 +0200

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on("connection", async (ws)=> {
     ws.on("message", async (message) => {
         console.log("received: \n %s", message);
-        ws.send(message);
+        ws.send(`${message}`);
     });
 });
 wss.on("listening", () => {
