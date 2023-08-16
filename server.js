64ceea49544e3d05e13d40b15f33e450a40a0e30
Patryk360 <nexigamingyt@gmail.com>
2023-08-17 00:17:13 +0200

    fix

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
-wss.on("connection", async (ws)=> {
+wss.on("connection", async (ws) => {
+    const data = {
+        type: "joinMessage",
+        text: "Hello from server",
+    }
+    ws.send(JSON.stringify(data));
+    console.log("New client connected");
+    
+    for (const client of wss.clients) {
+        client.send("ok");
+    }
+
     ws.on("message", async (message) => {
+        for (const client of wss.clients) {
+            client.send(`${message}`);
+        }
         console.log("received: \n %s", message);
-        ws.send(`${message}`);
     });
 });
 wss.on("listening", () => {
