f63309aa71364cc5507a91cace2e8f110922e6d8
Patryk360 <nexigamingyt@gmail.com>
2025-07-25 12:19:17 +0200

    0.0.11

--- wsFunctions/wsPlayerJoin.js
+++ wsFunctions/wsPlayerJoin.js
@@ -1,4 +1,17 @@
 "use strict";
-module.exports = (data, wss, ws) => {
+module.exports = async (data, wss, ws, gameServer) => {
     console.log(data);
+
+    const player = await gameServer.db.collection("Players").findOne({ _id: data.PlayerID });
+
+    const playerData = {
+        _id: data.PlayerID,
+        data: [
+            { name: "0001" }
+        ]
+    }
+
+    if (!player) await gameServer.db.collection("Players").insertOne(playerData);
+
+    console.log(player);
 }
--- wsFunctions/wsRealtimeChat.js
+++ wsFunctions/wsRealtimeChat.js
@@ -2,22 +2,9 @@
 module.exports = async (data, wss, ws, gameServer) => {
     console.log(data);
 
-    const player = await gameServer.db.collection("Players").findOne({ _id: 1111 });
-
-    const playerData = {
-        _id: 1111,
-        data: [
-            { content: "0001" }
-        ]
-    }
-
-    if (!player) await gameServer.db.collection("Players").insertOne(playerData);
-
-    console.log(player);
-
     wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify(player));
+          client.send(JSON.stringify(data.Content));
         }
     });
 }
